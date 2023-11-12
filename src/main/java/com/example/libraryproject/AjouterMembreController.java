b441e1d791d4fc590a89eed2d21d0b73f7edefc6
Oussama Naji <oussama.naji@student-cs.fr>
2023-11-13 00:57:17 +0100

    Derniers changements (2)

--- src/main/java/com/example/libraryproject/AjouterMembreController.java
+++ src/main/java/com/example/libraryproject/AjouterMembreController.java
@@ -10,6 +10,7 @@ import javafx.stage.Stage;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
@@ -39,14 +40,15 @@ public class AjouterMembreController {
         String mnom = nom.getText();
         String mprenom = prenom.getText();
 
+
         if (mnom.isEmpty() || mprenom.isEmpty()) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Message d'erreur");
             alert.setHeaderText(null);
             alert.setContentText("Veuillez remplir tous les champs vides.");
             alert.showAndWait();
+            return; // Sortez de la méthode si les champs sont vides
         }
-
 
         Connection connection = SqlController.connectDB();
 
@@ -55,6 +57,25 @@ public class AjouterMembreController {
             try {
                 int randomStudentNumber = (int) (Math.random() * 9000000) + 1000000;
                 String mStudentNumber = String.valueOf(randomStudentNumber);
+                // Vérifiez si le membre avec ce StudentNumber existe déjà
+                String checkMemberQuery = "SELECT COUNT(*) FROM student WHERE StudentNumber = ?";
+                PreparedStatement checkMemberStatement = connection.prepareStatement(checkMemberQuery);
+                checkMemberStatement.setString(1, mStudentNumber);
+
+                ResultSet result = checkMemberStatement.executeQuery();
+
+                if (result.next() && result.getInt(1) > 0) {
+                    // Le membre existe déjà
+                    alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Message d'erreur");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Un membre avec ce numéro étudiant existe déjà.");
+                    alert.showAndWait();
+                    return; // Sortez de la méthode si le membre existe déjà
+                }
+
+
+
                 // Créez la requête SQL d'insertion
                 String insertQuery = "INSERT INTO student (Nom, Prénom, password, StudentNumber) " +
                         "VALUES (?, ?, LEFT(MD5(CONCAT(Nom, Prénom, NOW())), 8), ?)";
@@ -63,10 +84,8 @@ public class AjouterMembreController {
                 preparedStatement.setString(2, mprenom);
                 preparedStatement.setString(3, mStudentNumber);
 
-
                 // Exécutez la requête
                 int rowsAffected = preparedStatement.executeUpdate();
-
 
                 if (rowsAffected > 0) {
                     alert = new Alert(Alert.AlertType.INFORMATION);
@@ -98,6 +117,7 @@ public class AjouterMembreController {
 
         }
     }
+
 
     @FXML
     private void cancel(ActionEvent event) {
