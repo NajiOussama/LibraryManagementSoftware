e573d6e985976251dcce7bccc658e1e6c7e91a0b
Oussama Naji <oussama.naji@student-cs.fr>
2023-11-14 07:38:47 +0100

    Derniers changements (prez)

--- src/main/java/com/example/libraryproject/EmprunterController.java
+++ src/main/java/com/example/libraryproject/EmprunterController.java
@@ -64,92 +64,107 @@ public class EmprunterController {
             checkStatement.setString(1, mstudnumber);
             checkStatement.setString(2, mpass);
             var res = checkStatement.executeQuery();
-            if (res.next()) {
-                try {
-                    // Vérifier si le livre est disponible
-                    String checkAvailabilityQuery = "SELECT Disponible FROM Livre WHERE ISBN = ?";
-                    PreparedStatement checkAvailabilityStatement = connection.prepareStatement(checkAvailabilityQuery);
-                    checkAvailabilityStatement.setString(1, misbn);
-                    var availabilityResult = checkAvailabilityStatement.executeQuery();
 
-                    if (availabilityResult.next() && availabilityResult.getBoolean("Disponible")) {
-                        String checkEmpruntQuery = "SELECT * FROM Emprunts WHERE livre_id = ? AND student_id = (SELECT ID FROM student WHERE StudentNumber=? and password=?) ";
-                        PreparedStatement checkEmpruntStatement = connection.prepareStatement(checkEmpruntQuery);
-                        checkEmpruntStatement.setString(1, misbn);
-                        checkEmpruntStatement.setString(2, mstudnumber);
-                        checkEmpruntStatement.setString(3, mpass);
+            String verifLivre = "SELECT * FROM Livre WHERE Titre =? and ISBN = ?";
+            PreparedStatement authenticateStatement = connection.prepareStatement(verifLivre);
+            authenticateStatement.setString(1,mtit );
+            authenticateStatement.setString(2, misbn);
+            ResultSet res2 = authenticateStatement.executeQuery();
+            if (res.next() ) {
+                if (res2.next()) {
+                    try {
+                        // Vérifier si le livre est disponible
+                        String checkAvailabilityQuery = "SELECT Disponible FROM Livre WHERE ISBN = ?";
+                        PreparedStatement checkAvailabilityStatement = connection.prepareStatement(checkAvailabilityQuery);
+                        checkAvailabilityStatement.setString(1, misbn);
+                        var availabilityResult = checkAvailabilityStatement.executeQuery();
 
-                        ResultSet existingEmpruntResult = checkEmpruntStatement.executeQuery();
+                        if (availabilityResult.next() && availabilityResult.getBoolean("Disponible")) {
+                            String checkEmpruntQuery = "SELECT * FROM Emprunts WHERE livre_id = ? AND student_id = (SELECT ID FROM student WHERE StudentNumber=? and password=?) ";
+                            PreparedStatement checkEmpruntStatement = connection.prepareStatement(checkEmpruntQuery);
+                            checkEmpruntStatement.setString(1, misbn);
+                            checkEmpruntStatement.setString(2, mstudnumber);
+                            checkEmpruntStatement.setString(3, mpass);
 
-                        if (!existingEmpruntResult.next()) {
-                            // Mettre à jour la disponibilité du livre
-                            String updateAvailabilityQuery = "UPDATE Livre SET Disponible = 0 WHERE ISBN = ?";
-                            PreparedStatement updateAvailabilityStatement = connection.prepareStatement(updateAvailabilityQuery);
-                            updateAvailabilityStatement.setString(1, misbn);
-                            updateAvailabilityStatement.executeUpdate();
+                            ResultSet existingEmpruntResult = checkEmpruntStatement.executeQuery();
 
-                            // Insérer l'emprunt dans la table Emprunts
-                            String insertEmpruntQuery = "INSERT INTO Emprunts (livre_id, student_id) " +
-                                    "VALUES (?, (SELECT ID FROM student WHERE StudentNumber = ?))";
-                            PreparedStatement insertEmpruntStatement = connection.prepareStatement(insertEmpruntQuery);
-                            insertEmpruntStatement.setString(1, misbn);
-                            insertEmpruntStatement.setString(2, mstudnumber);
+                            if (!existingEmpruntResult.next()) {
+                                // Mettre à jour la disponibilité du livre
+                                String updateAvailabilityQuery = "UPDATE Livre SET Disponible = 0 WHERE ISBN = ?";
+                                PreparedStatement updateAvailabilityStatement = connection.prepareStatement(updateAvailabilityQuery);
+                                updateAvailabilityStatement.setString(1, misbn);
+                                updateAvailabilityStatement.executeUpdate();
 
-                            // Exécuter la requête
-                            int rowsAffected = insertEmpruntStatement.executeUpdate();
+                                // Insérer l'emprunt dans la table Emprunts
+                                String insertEmpruntQuery = "INSERT INTO Emprunts (livre_id, student_id) " +
+                                        "VALUES (?, (SELECT ID FROM student WHERE StudentNumber = ?))";
+                                PreparedStatement insertEmpruntStatement = connection.prepareStatement(insertEmpruntQuery);
+                                insertEmpruntStatement.setString(1, misbn);
+                                insertEmpruntStatement.setString(2, mstudnumber);
 
-                            if (rowsAffected > 0) {
-                                String insertHistoriqueQuery = "INSERT INTO Historique (date_pret, date_retour, student_id, livre_id) VALUES (CURDATE(),null, (SELECT ID FROM student WHERE StudentNumber = ?), ?)";
-                                PreparedStatement insertHistoriqueStatement = connection.prepareStatement(insertHistoriqueQuery);
+                                // Exécuter la requête
+                                int rowsAffected = insertEmpruntStatement.executeUpdate();
 
-                                insertHistoriqueStatement.setString(1, mstudnumber);
-                                insertHistoriqueStatement.setString(2, misbn);
-                                insertHistoriqueStatement.executeUpdate();
+                                if (rowsAffected > 0) {
+                                    String insertHistoriqueQuery = "INSERT INTO Historique (date_pret, date_retour, student_id, livre_id) VALUES (CURDATE(),null, (SELECT ID FROM student WHERE StudentNumber = ?), ?)";
+                                    PreparedStatement insertHistoriqueStatement = connection.prepareStatement(insertHistoriqueQuery);
 
-                                alert = new Alert(Alert.AlertType.INFORMATION);
-                                alert.setTitle("Admin Message");
-                                alert.setHeaderText(null);
-                                alert.setContentText("Livre emprunté avec succès.");
-                                alert.showAndWait();
+                                    insertHistoriqueStatement.setString(1, mstudnumber);
+                                    insertHistoriqueStatement.setString(2, misbn);
+                                    insertHistoriqueStatement.executeUpdate();
+
+                                    alert = new Alert(Alert.AlertType.INFORMATION);
+                                    alert.setTitle("Admin Message");
+                                    alert.setHeaderText(null);
+                                    alert.setContentText("Livre emprunté avec succès.");
+                                    alert.showAndWait();
+                                } else {
+                                    alert = new Alert(Alert.AlertType.ERROR);
+                                    alert.setTitle("Message d'erreur");
+                                    alert.setHeaderText(null);
+                                    alert.setContentText("Échec de l'emprunt du livre.");
+                                    alert.showAndWait();
+                                }
                             } else {
                                 alert = new Alert(Alert.AlertType.ERROR);
                                 alert.setTitle("Message d'erreur");
                                 alert.setHeaderText(null);
-                                alert.setContentText("Échec de l'emprunt du livre.");
+                                alert.setContentText("Vous avez déjà emprunté ce livre.");
                                 alert.showAndWait();
+
                             }
-                        }else {
+
+                        } else {
                             alert = new Alert(Alert.AlertType.ERROR);
                             alert.setTitle("Message d'erreur");
                             alert.setHeaderText(null);
-                            alert.setContentText("Vous avez déjà emprunté ce livre.");
+                            alert.setContentText("Échec de l'emprunt du livre");
                             alert.showAndWait();
-
                         }
 
-                    } else {
+                    } catch (SQLException e) {
+                        e.printStackTrace();
                         alert = new Alert(Alert.AlertType.ERROR);
                         alert.setTitle("Message d'erreur");
                         alert.setHeaderText(null);
-                        alert.setContentText("Échec de l'emprunt du livre");
+                        alert.setContentText("Échec de l'emprunt du livre.");
                         alert.showAndWait();
+                    } finally {
+                        try {
+                            if (connection != null) {
+                                connection.close();
+                            }
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
                     }
-
-                } catch (SQLException e) {
-                    e.printStackTrace();
+                }else{
                     alert = new Alert(Alert.AlertType.ERROR);
                     alert.setTitle("Message d'erreur");
                     alert.setHeaderText(null);
-                    alert.setContentText("Échec de l'emprunt du livre.");
+                    alert.setContentText("Titre ou ISBN incorrect.");
                     alert.showAndWait();
-                } finally {
-                    try {
-                        if (connection != null) {
-                            connection.close();
-                        }
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
+
                 }
 
             }else {
